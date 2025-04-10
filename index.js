a4a80de33cbcaead8a1d02cf10cf78bd491fd6c1
Wassim Affes <wassimaffes947@gmail.com>
2025-04-10 21:34:31 +0100

    mise a jour back

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
